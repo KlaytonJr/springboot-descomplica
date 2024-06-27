0ef3329f396dbee4ebe805738a3c6f4c40fcafc8
KlaytonJr <klaytonjunior@protonmail.com>
2024-06-26 22:56:43 -0300

    feat: create upload/download service

--- api/src/main/java/com/example/api/service/FileService.java
+++ api/src/main/java/com/example/api/service/FileService.java
@@ -1,13 +1,82 @@
 package com.example.api.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.api.config.FileStorageProperties;
+import com.example.api.exception.FileNotFoundException;
+import com.example.api.exception.UploadFileException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class FileService {
 
+    private final Path fileStorageLocation;
+
     public FileService(FileStorageProperties fileStorageProperties) {
-        fileStorageProperties.getUploadDir();
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(fileStorageLocation);
+        } catch (IOException e) {
+            throw new UploadFileException("Something went wrong when trying to create the folder");
+        }
+    }
+
+    public String uploadFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new UploadFileException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (UploadFileException | IOException e) {
+            throw new UploadFileException("Error when trying to save file", e);
+        }
+    }
+
+    public Resource downloadFile(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException("File not found");
+            }
+        } catch (FileNotFoundException | MalformedURLException e) {
+            throw new FileNotFoundException("File not found");
+        }
+    }
+
+    public String getContentType(HttpServletRequest request, Resource resource) {
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            log.error("Unable to determine file type");
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return contentType;
     }
 }
