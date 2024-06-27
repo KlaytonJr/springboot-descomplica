ebe2d07711456e774de548d7ba375cb0ee033abd
KlaytonJr <klaytonjunior@protonmail.com>
2024-06-26 23:08:14 -0300

    feat: create upload/download controller

---
+++ api/src/main/java/com/example/api/controller/FileController.java
@@ -0,0 +1,49 @@
+package com.example.api.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.api.entity.File;
+import com.example.api.service.FileService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    private FileService fileService;
+
+    @PostMapping("/upload")
+    public File uploadFile (@RequestParam("file") MultipartFile file) {
+        String fileName = fileService.uploadFile(file);
+
+        String filePath = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("files/dowload")
+            .path(fileName)
+            .toUriString();
+
+        return new File(fileName, filePath, file.getContentType(), file.getSize());
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> downloadFile (String fileName, HttpServletRequest request) {
+
+        Resource resource = fileService.downloadFile(fileName);
+
+        String contentType = fileService.getContentType(request, resource);
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+            .body(resource);
+    }
+}
