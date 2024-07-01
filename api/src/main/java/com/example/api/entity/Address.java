76f048167f19417bd6f8072e63d428e6b8aa6c63
KlaytonJr <klaytonjunior@protonmail.com>
2024-07-01 19:43:58 -0300

    feat: create one to one relation of Student between Address

--- api/src/main/java/com/example/api/entity/Address.java
+++ api/src/main/java/com/example/api/entity/Address.java
@@ -1,9 +1,12 @@
 package com.example.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +24,9 @@ public class Address {
     private String city;
     private String state;
     private String zipCode;
+    private String neighborhood;
+
+    @OneToOne(mappedBy="address")
+    @JsonIgnore
+    private Student student;
 }
--- api/src/main/java/com/example/api/entity/Student.java
+++ api/src/main/java/com/example/api/entity/Student.java
@@ -2,10 +2,13 @@ package com.example.api.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,5 +27,9 @@ public class Student {
     private String name;
     private String email;
     private LocalDate birthDate;
+
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="address_id", referencedColumnName="id")
+    private Address address;
 
 }
